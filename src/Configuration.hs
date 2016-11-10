86f1e4bc37c95becf51f35a11a2bc17d19965f8c
Steven Collins <scollins@xogrp.com>
2016-11-10 16:00:09 -0600

    Fix imports - add Data.Text.

--- src/Configuration.hs
+++ src/Configuration.hs
@@ -8,7 +8,7 @@ module Configuration
   ) where
 
 import Network.AMQP(ConnectionOpts(..), plain, defaultConnectionOpts)
-import Data.Text(Text)
+import Data.Text
 
 xoConnectionOpts :: ConnectionOpts
 xoConnectionOpts = defaultConnectionOpts {
