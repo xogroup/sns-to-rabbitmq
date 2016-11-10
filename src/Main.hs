a6e4f4dc55ce444d22661330e4825cfaa63839bf
Steven Collins <scollins@xogrp.com>
2016-11-10 16:00:45 -0600

    Commit first real working version.

--- src/Main.hs
+++ src/Main.hs
@@ -7,24 +7,44 @@ import Configuration
 import Control.Concurrent
 import System.Posix.Signals
 import Control.Exception
+import Data.Pool
+import Data.Word
+import Data.Text.Encoding
+import Control.Monad
 
-mkApp :: Connection -> Application
-mkApp conn = \ req respond -> do
-  chan <- openChannel conn
-  respond $ responseLBS
-    status200
-    [("Content-Type", "text/plain")]
-    "Hello, Web!"
+maxChannels :: Word16
+maxChannels = 10000
+
+mkApp :: Pool Channel -> Application
+mkApp pool = \ req respond -> do
+  msg <- strictRequestBody req
+  let contentType = (liftM decodeUtf8) $
+                    lookup "Content-Type" $
+                    requestHeaders req
+  _ <- safeRabbitAction pool $
+    \chan ->
+      publishMsg chan xoExchange xoKey
+      newMsg { msgBody = msg
+             , msgDeliveryMode = Just Persistent
+             , msgContentType = contentType}
+  respond $ responseLBS status200 [] ""
 
 main :: IO ()
 main = do
   -- main is mostly cleanup/handler stuff
-  connContainer <- newEmptyMVar
   originalHandlerContainer <- newEmptyMVar
 
+  -- connPool: one "stripe" of ten connections.
+  connPool <- createPool (openConnection'' xoConnectionOpts)
+              closeConnection 1 5 10
+
+  -- chanPool: many "stripes" but only one channel per stripe, because
+  -- each thread should only need one channel.
+  chanPool <- createPool (safeRabbitAction connPool getConfirmModeChan)
+              closeChannel (fromIntegral maxChannels) 600 1
+
   let handler = do
-        conn <- takeMVar connContainer
-        closeConnection conn
+        destroyAllResources connPool
         original <- takeMVar originalHandlerContainer
         -- reinstall the original handler and immediately invoke it
         _ <- (installHandler sigTERM original Nothing)
@@ -32,19 +52,47 @@ main = do
 
   originalHandler <- (installHandler sigTERM (Catch handler) Nothing)
   putMVar originalHandlerContainer originalHandler
-  bracket
-    (openConnection'' xoConnectionOpts)
-    closeConnection
-    (\conn -> do
-        -- store the connection somewhere the signal handler can get to it
-        putMVar connContainer conn
 
-        -- we'll use a fresh channel for each request, but first we need
-        -- to set up the queue and exchange
-        chan <- openChannel conn
-        _ <- declareQueue chan newQueue {queueName = xoQueue}
-        declareExchange chan newExchange { exchangeName = xoExchange
-                                         , exchangeType = "direct"}
-        bindQueue chan xoQueue xoExchange xoKey
+  finally
+    (do
+        safeRabbitAction chanPool initializeQueue
+        run 3000 (mkApp chanPool))
+    (destroyAllResources connPool)
 
-        run 3000 (mkApp conn))
+-- | Get a channel and put it in publisher confirm mode. This function may
+-- fail and/or throw an exception.
+getConfirmModeChan :: Connection -> IO Channel
+getConfirmModeChan conn = do
+  chan <- openChannel conn
+  confirmSelect chan False
+  return chan
+
+-- | Initialize the configured queue and exchange. This function may fail
+-- and/or throw an exception.
+initializeQueue :: Channel -> IO ()
+initializeQueue chan = do
+  _ <- declareQueue chan newQueue {queueName = xoQueue}
+  declareExchange chan newExchange { exchangeName = xoExchange
+                                   , exchangeType = "direct"}
+  bindQueue chan xoQueue xoExchange xoKey
+
+-- the exception predicate to pass to tryJust in safeRabbitAction.
+selectClosedExceptions :: AMQPException -> Maybe AMQPException
+selectClosedExceptions ex = case ex of
+  e@(ChannelClosedException _) -> Just e
+  e@(ConnectionClosedException _) -> Just e
+  _ -> Nothing
+
+-- | Perform an action repeatedly until no ChannelClosedException
+-- or ConnectionClosedException occurs.
+safeRabbitAction :: Pool b -> (b -> IO a) -> IO a
+safeRabbitAction pool f =
+  do r <- tryJust selectClosedExceptions $
+          withResource pool f
+     -- by Haskell convention, 'Left e' means an error 'e' occurred; 'Right a'
+     -- means no error occurred and 'a' is the answer. Using 'withResource'
+     -- above ensures that the 'Connection' or 'Channel' involved is destroyed
+     -- if any exception whatsoever occurs.
+     case r of
+       Left _ -> safeRabbitAction pool f
+       Right a -> return a
